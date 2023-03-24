98753976b434360f81c2129ef284110b9e431b62
Candice Kohlbeck <123694352+kandiheart@users.noreply.github.com>
2023-03-24 18:23:25 -0500

    homepage update

--- client/src/Pages/Homepage.js
+++ client/src/Pages/Homepage.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Container, Box, Text, Tabs, Tab, TabPanels, TabList, TabPanel } from '@chakra-ui/react';
+import { Container, Box, Text, Tabs, Tab, TabPanels, TabList, TabPanel, AbsoluteCenter } from '@chakra-ui/react';
 import Login from "../components/Auth/Login";
 import Signup from "../components/Auth/Signup";
 
 const Homepage = () => {
-    return <Container maxW="xl" centerContent>
+    return <AbsoluteCenter w={"50%"} h={"50%"}>
+    <Container maxW="xl" centerContent>
         <Box display="flex" justifyContent="center" p={3} bg={"white"} w="100%" m="40px 0 15px 0" borderRadius="lg" borderWidth="1px">
             <Text fontSize={'4xl'} fontFamily='Orbitron' color={"black"}>Chat-App</Text>
         </Box>
@@ -24,7 +25,8 @@ const Homepage = () => {
             </TabPanels>
             </Tabs>
         </Box>
-    </Container>;
+    </Container>
+    </AbsoluteCenter>;
 };
 
 export default Homepage;
