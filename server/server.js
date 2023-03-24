1829e1b1a84b51ad226ba526f800ef2d87fb8c41
Candice Kohlbeck <123694352+kandiheart@users.noreply.github.com>
2023-03-24 14:13:57 -0500

    server working

--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 
-const app = express()
-
+const app = express();
+dotenv.config();
 
 app.get("/", (req,res)=>{
-    res.send("API is running");
+    res.send("API is running successfully");
 });
 app.get("/api/chat", (req,res)=>{
     res.send(chats);
 });
+app.get("/api/chat/:id", (req, res)=>{
+   // console.log(req.params.id);
+   const singleChat = chats.find(c=>c._id === req.params.id);
+   res.send(singleChat);
+});
 
-
-app.listen(5000,console.log("server started on port 5000"));
+const PORT = process.env.PORT || 5000
+app.listen(PORT,console.log(`server started on port ${PORT}`));
