37c5e6b9899880be286596e30a23613dba75ca6e
Candice Kohlbeck <123694352+kandiheart@users.noreply.github.com>
2023-03-24 13:56:18 -0500

    start server data

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express()
+app.get('/', (req,res)=>{
+    res.send("API is running");
+});
+app.get('/api/chat', (req,res)=>{
+    res.send(chats);
+});
+app.listen(5000,console.log("server started on port 5000"));
