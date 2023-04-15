6add31bda57fb79bed90d13c6656311d6b82f7dd
Candice Kohlbeck <123694352+kandiheart@users.noreply.github.com>
2023-04-15 15:32:28 -0500

    crud for group chats

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const {protect} = require("../middleware/authMiddleware");
-const {accessChat} = require("../controllers/chatControllers");
+const { protect } = require("../middleware/authMiddleware");
+const { accessChat, fetchChats, createGroupChat, renameGroup, addToGroup, removeFromGroup } = require("../controllers/chatControllers");
 
 router.route('/').post(protect, accessChat);
-// router.route('/').get(protect, fetchChats);
-// router.route('/group').post(protect, createGroupChat);
-// router.route('/rename').put(protect, renameGroup);
-// router.route('/groupremove').put(protect, removeFromGroup);
-// router.route('/groupadd').put(protect, addToGroup);
+router.route('/').get(protect, fetchChats);
+router.route('/group').post(protect, createGroupChat);
+router.route('/rename').put(protect, renameGroup);
+router.route('/groupadd').put(protect, addToGroup);
+router.route('/groupremove').put(protect, removeFromGroup);
+
 
 module.exports = router;
